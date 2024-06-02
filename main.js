d3dcb6084659fede6430341465af06508e3c477c
Martin Solari <martinsolari@gmail.com>
2024-06-02 11:22:41 -0300

    load book inventory

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-import Book from './book.js';
+import Library from './library.js';
 
-const myBook = new Book('Cuentos de la Selva', "Horacio Quiroga", 350);
+const myLibrary = new Library('Montevideo');
+myLibrary.addBook('Cuentos de la Selva', "Horacio Quiroga", 350);
 
-console.log(`Title: ${myBook.getTitle()}`);
-console.log(`Author: ${myBook.getAuthor()}`);
-console.log(`Pages: ${myBook.getPages()}`);
+function loadInventory(newBook) {
+  const emptyBookList = document.getElementById("empty-book-list");
+  const bookListContainer = document.getElementById("book-list-container");
+  const bookList = document.getElementById("book-list");
+  
+  emptyBookList.classList.add('d-none');
+  bookListContainer.classList.remove("d-none");
+  let newListElement = document.createElement("li");
+  newListElement.classList.add("list-group-item");
+  newListElement.innerText = newBook.toString();
+  bookList.appendChild(newListElement);
+}
+
+loadInventory(myLibrary.books[0]);
