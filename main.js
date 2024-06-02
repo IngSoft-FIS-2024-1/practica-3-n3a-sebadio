71eb459a6544b591e24fb41a4a4d54d654de88b9
Martin Solari <martinsolari@gmail.com>
2024-06-02 18:28:54 +0000

    add book event

--- main.js
+++ main.js
@@ -16,4 +16,27 @@ function loadInventory(newBook) {
   bookList.appendChild(newListElement);
 }
 
+const btnAdd = document.getElementById('btn-add');
+const inpTitle = document.getElementById('inp-title');
+const inpAuthor = document.getElementById('inp-author');
+const inpPages = document.getElementById('inp-pages');
+
+btnAdd.addEventListener('click', () => {
+  const newBook = new Book(inpTitle.value, inpAuthor.value, inpPages.value);
+  const bookErrorContainer = document.getElementById("add-book-error");
+  const bookError = document.getElementById("add-book-error-msg");
+  try
+  {
+    myLibrary.addBook(newBook);
+    clearInputs();
+    countriesErrorContainer.classList.add("d-none");
+    loadbookList(newBook);
+  }
+  catch (error)
+  {
+    bookErrorContainer.classList.remove("d-none");
+    bookError.innerText = error;
+  }
+});
+
 loadInventory(myLibrary.books[0]);
