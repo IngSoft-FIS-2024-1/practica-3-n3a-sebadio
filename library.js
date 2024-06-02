d45fdbb2b3e7bf1480971610b2b682a5a087fbfa
Martin Solari <martinsolari@gmail.com>
2024-06-02 08:29:51 -0300

    library tests

--- library.js
+++ library.js
@@ -1,12 +1,28 @@
 import Book from './book.js'
 class Library {
-    books = [];
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+    setName(name) {
+        if (typeof (name) !== 'string') {
+            throw new Error();
+        }
+        name = name.trim();
+        if (name.length === 0) {
+            throw new Error();
+        }
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
     addBook(title, author, pages) {
-        newBook = new Book(title, author, pages);
+        const newBook = new Book(title, author, pages);
         this.books.push(newBook);
     }
     totalBooks() {
-        return books.lenght;
+        return this.books.lenght;
     }
 }
 export default Library;
---
+++ library.test.js
@@ -0,0 +1,33 @@
+import Library from './library.js';
+import Book from './book.js';
+
+describe('Library', () => {
+  let myLibrary;
+
+  beforeEach(() => {
+    myLibrary = new Library();
+  });
+
+  it('should add a book to the library', () => {
+    myLibrary.addBook('Cuentos de la Selva', 'Horacio Quiroga', 120);
+    expect(myLibrary.books[0]).toBeInstanceOf(Book);
+    expect(myLibrary.books[0].getTitle()).toBe('Cuentos de la Selva');
+  });
+
+  it('should return the total number of books', () => {
+    myLibrary.addBook('Cuentos de la Selva', 'Horacio Quiroga', 120);
+    myLibrary.addBook('El Hombre que Calculaba', 'Malba Tahan', 286);
+    expect(myLibrary.totalBooks()).toBe(2);
+  });
+
+  it('should set the name of the library', () => {
+    myLibrary.setName('Montevideo');
+    expect(myLibrary.getName()).toBe('Montevideo');
+  });
+  
+  it('should throw an error when setting an invalid name', () => {
+    expect(() => myLibrary.setName('')).toThrow();
+    expect(() => myLibrary.setName(123)).toThrow();
+  });
+  
+});
