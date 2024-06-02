2f96fb4a6c86c8a5dd5218b28755d3481579c9d4
Martin Solari <martinsolari@gmail.com>
2024-06-02 10:48:12 +0000

    set methods

--- book.js
+++ book.js
@@ -1,20 +1,8 @@
 class Book {
   constructor(title, author, pages) {
-    if (typeof (title) !== 'string') {
-      throw new Error();
-    }
-    if (title.trim().length === 0) {
-      throw new Error();
-    }
-    if (typeof (pages) !== 'number' && !isNan(pages)) {
-      throw new Error();
-    }
-    if (pages < 1) {
-      throw new Error();
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+    this.setTitle(title);
+    this.setAuthor(author);
+    this.setPages(pages);
   }
 
   getTitle() {
@@ -27,6 +15,36 @@ class Book {
 
   getPages() {
     return this.pages;
+  }
+
+  setTitle(title) {
+    if (typeof (title) !== 'string') {
+      throw new Error();
+    }
+    title = title.trim();
+    if (title.length === 0) {
+      throw new Error();
+    }
+    this.title = title;
+  }
+
+  setAuthor(author) {
+    if (typeof (author) !== 'string') {
+      throw new Error();
+    }
+    author = author.trim();
+    this.author = author;
+  }
+
+  setPages(pages) {
+    if (typeof (pages) !== 'number' && !isNaN(pages)) {
+      throw new Error();
+    }
+    if (pages < 1) {
+      throw new Error();
+    }
+    pages = Math.trunc(pages);
+    this.pages = pages;
   }
 }
 
